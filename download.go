ded3a7332d5e439ad4e145d1beb0b4a145e696ad
Gianluca Bortoli <33416683+cog-qlik@users.noreply.github.com>
2019-07-23 11:48:56 +0200

    Add HTTP header RFC comment

--- download.go
+++ download.go
@@ -17,7 +17,8 @@ func Download(URL string) ([]byte, error) {
 	if resp.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("download of %s returned %d status code", URL, resp.StatusCode)
 	}
-
+	// RFC 2616 and RFC 7230 define HTTP headers as case insensitive.
+	// Transform it in lowercase to make comparison work in all the possible scenarios. 
 	contentType := resp.Header.Get("Content-Type")
 	if strings.ToLower(contentType) != "text/html; charset=utf-8" {
 		return nil, fmt.Errorf("wrong content type %s", contentType)
